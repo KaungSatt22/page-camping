089f19861217a3fbe746e886a9227d30361f3cf7
Kaung Satt <kaungsatt.2k1@gmail.com>
2024-04-09 08:44:15 +0630

    modified responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
+
+let mainHeader = document.querySelector(".l-header");
+let menu = document.querySelector(".l-header__hamburger");
+menu.addEventListener("click", function () {
+  mainHeader.classList.toggle("show");
+});
